9d2a18824527d10bcd1e21ed3b82bdb82c30e8d1
Nicholas Cruz <NICruz@student.fullsail.edu>
2023-05-02 18:31:55 -0400

    Establish MySQL database connection with Sequelize

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const path = require("path");
+const { Sequelize } = require('sequelize');
+const mysql=require("mysql2");
 require("dotenv").config();
 
 app.use(express.json())
@@ -9,6 +11,14 @@ app.use(express.json())
     app.use(express.static(path.join(__dirname, "../exercise-buddy/build")));
 
     app.get('/*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+
+const sequelize = new Sequelize("exercise_buddy", "root", "root", {host:"localhost", dialect:"mysql"});
+try {
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
 
 
 app.listen(process.env.port || 3030, ()=>{
