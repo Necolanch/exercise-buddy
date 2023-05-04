9dce4c9f7d260c1c0922420d50d6289953c9e5fd
Nicholas Cruz <NICruz@student.fullsail.edu>
2023-05-04 18:28:23 -0400

    Create basic routes and controllers for user model

---
+++ api/routes/userRoutes.js
@@ -0,0 +1,8 @@
+const express =require("express");
+const router=express.Router();
+const userControl=require("../controllers/userController");
+
+router.get("/:id", userControl.find);
+router.post("/create", userControl.create);
+router.post("/:id/update", userControl.update);
+router.delete("/:id/delete", userControl.remove);
