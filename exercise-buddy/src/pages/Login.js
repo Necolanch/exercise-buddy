c90ca37335998e3b9762416080ca0e2b3b9a9fee
Nicholas Cruz <NICruz@student.fullsail.edu>
2023-05-18 13:19:30 -0400

    Connect login page to server and update response values

--- exercise-buddy/src/pages/Login.js
+++ exercise-buddy/src/pages/Login.js
@@ -1,16 +1,34 @@
 import { Box, FormControl, TextField } from "@mui/material";
-import React from "react";
+import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
 import { ActionButton } from "../Components/Button";
+import authService from "../services/auth.service";
 
 const Login=()=>{
+    const [username, setUsername]=useState("");
+    const [password, setPassword]=useState("");
+    const navigate=useNavigate();
+
+    const handleLogin=async(e)=>{
+        e.preventDefault();
+        try {
+            await authService.login(username, password)
+            .then(response=>{
+                localStorage.setItem("user", JSON.stringify(response.user));
+                navigate("/home");
+            })
+        } catch (error) {
+            console.error(error)
+        }
+    }
     return(
         <Box sx={{width:"100vw", height:"100vh",margin:"0 auto", display:"flex", flexDirection:"column", alignItems:"center"}}>
             <img className="mb-1" src={require("../IMG/EB-logo.png")} alt="Exercise Buddy Logo" width="50" height="50"/>
 
         <FormControl>
-            <TextField id="username" label="Username"/>
-            <TextField id="password" label="Password"/>
-            <ActionButton variant="contained" text="Login"/>
+            <TextField onChange={e=>setUsername(e.target.value)} id="username" label="Username" type="text"/>
+            <TextField onChange={e=>setPassword(e.target.value)} id="password" label="Password" type="password"/>
+            <ActionButton action={handleLogin} variant="contained" text="Login"/>
         </FormControl>
 
         </Box>
