045b0299d28ca3802c88c1bc68bcc97f1616b46e
Nicholas Cruz <necolanch@gmail.com>
2024-11-01 14:31:17 -0400

    Update localhost ports on frontend services to target api container exposed port

--- exercise-buddy/src/services/auth.service.js
+++ exercise-buddy/src/services/auth.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_BASE=process.env.NODE_ENV==="production" ? process.env.REACT_APP_BASE_URL : "http://localhost:3030" 
+const API_BASE=process.env.NODE_ENV==="production" ? process.env.REACT_APP_BASE_URL : "http://localhost:8000" 
 
 const signup=(username, password, confirm)=>{
     const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&{}'"()^#|:;.,<>/?`~_-])[A-Za-z\d@$!%*?&{}'"()^#|:;.,<>/?`~_-}]{8,}$/;
